40496d2d2467dbb32a1258d8a507ef579a550aa3
taufiqaa <mo.taufiqaa@gmail.com>
2022-07-10 00:16:56 +0700

    updated

--- assets/javascript/blog.js
+++ assets/javascript/blog.js
@@ -4,17 +4,33 @@ function addProject(event) {
     event.preventDefault()
 
     let projectName = document.getElementById("project_name").value;
+    let startDate = document.getElementById("start_date").value;
+    let endDate = document.getElementById("end_date").value;
     let description = document.getElementById("description").value;
     let imageFile = document.getElementById("upload_image_button");
+
+    if(!projectName && !startDate &&!endDate && !description){
+        alert("All fields are required");
+    }else if(!projectName){
+        alert("Project Name is required");
+    }else if(!startDate){
+        alert("Start Date is required");
+    }else if(!endDate){
+        alert("End Date is required");
+    }else if(!description){
+        alert("Description is required");
+    }
     
-  
- 
         imageFile = URL.createObjectURL(imageFile.files[0])
+        let duration = timeDuration(new Date(startDate), new Date(endDate));
+        let languages = checkboxIsSelected(); 
     //imageFile.srcObject = imageFile.files[0]
     let project = {
         projectName,
         description,
-        imageFile
+        imageFile,
+        duration,
+        languages,
     }
    console.table(imageFile);
 
@@ -22,16 +38,31 @@ function addProject(event) {
     loadProjects()
 }
 
+function timeDuration(startDate,endDate){
+    let durationResult = startDate.getMonth() - endDate.getMonth() + 12 * (endDate.getFullYear() - startDate.getFullYear());
+
+    return Math.abs(durationResult);
+}
+
+function checkboxIsSelected(){
+    let checkboxActivated = document.querySelectorAll(".language_checkbox input[type='checkbox']:checked");
+
+    let addCheckbox = []
+    
+    for(let i=0; i<checkboxActivated.length; i++){
+        addCheckbox.push(checkboxActivated[i].value);
+    }
+    return addCheckbox;
+}
+
 function loadProjects() {
    console.log(projects);
-
-  
     let containerProjects = document.getElementById("big_content")
     
     containerProjects.innerHTML = ""
 
     for (let i = 0; i < projects.length; i++) {
-        
+       let languageType = projects[i].languages;
         containerProjects.innerHTML += `
     <div class="box" id="box_project">
         <div class="image_content">
@@ -41,7 +72,7 @@ function loadProjects() {
             <h3>${projects[i].projectName}</h3>
         </div>
         <div id="duration">
-            <h4>Durasi</h4>  
+            <h6>Duration : ${projects[i].duration} month(s)</h6>  
         </div>
         <div id="output_description">
             <p>${projects[i].description}</p>
@@ -49,17 +80,16 @@ function loadProjects() {
         <div class="container_language_icon">
             <div class="flex_language_icon">
                 <div class="language_icon">
-                <img src="assets/images/node_js.png" style="display:none" alt="" id="node_js">
-                    
+                ${language_1.checked? `<img src="assets/images/node_js.png" alt="" id="node_js">`:""}
                 </div>
                 <div class="language_icon">
-                    <img src="assets/images/react_js.png" alt="" id="react_js">
+                ${language_2.checked? `<img src="assets/images/react_js.png" alt="" id="react_js">`:""}
                 </div>
                 <div class="language_icon">
-                    <img src="assets/images/vue_js.png" alt="" id="vue_js">
+                ${language_3.checked? `<img src="assets/images/vue_js.png" alt="" id="vue_js">`:""}
                 </div>
                 <div class="language_icon">
-                    <img src="assets/images/typescript.png" alt="" id="typescript">
+                ${language_4.checked? `<img src="assets/images/typescript.png" alt="" id="typescript">`:""}
                 </div>
             </div>
         </div>
@@ -72,21 +102,5 @@ function loadProjects() {
             </div>
         </div>
     </div>`
-
-    let Node_js = document.getElementById("node_js");
-    let checkbox_language_1 = document.getElementById("all_language_icon1");
-
-    checkbox_language_1.onsubmit = function(){
-        Node_js.style.display = "block";
-          }
-
-
-          
         }
     }
-
-    
-
-
-
-
