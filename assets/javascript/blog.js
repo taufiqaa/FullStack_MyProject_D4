04502ff2ccd67ea1763cbccae6ca44083992ed6c
taufiqaa <mo.taufiqaa@gmail.com>
2022-07-10 20:51:59 +0700

    updated

--- assets/javascript/blog.js
+++ assets/javascript/blog.js
@@ -1,4 +1,5 @@
 let projects = []
+let CheckboxIsSelected = [];
 
 function addProject(event) {
     event.preventDefault()
@@ -8,6 +9,7 @@ function addProject(event) {
     let endDate = document.getElementById("end_date").value;
     let description = document.getElementById("description").value;
     let imageFile = document.getElementById("upload_image_button");
+    
 
     if(!projectName && !startDate &&!endDate && !description){
         alert("All fields are required");
@@ -19,60 +21,49 @@ function addProject(event) {
         alert("End Date is required");
     }else if(!description){
         alert("Description is required");
+        return
     }
-    
+       
         imageFile = URL.createObjectURL(imageFile.files[0])
-        let duration = timeDuration(new Date(startDate), new Date(endDate));
-        let languages = checkboxIsSelected(); 
+        let languagesProject = document.getElementsByClassName("language_field");
+        
+        CheckboxIsSelected =[];
+        for (var i=0; i<languagesProject.length; i++){
+            if (languagesProject[i].checked == true) {
+                CheckboxIsSelected.push(languagesProject[i].value)
+            }
+        }
     //imageFile.srcObject = imageFile.files[0]
     let project = {
         projectName,
         description,
         imageFile,
-        duration,
-        languages,
-    }
-   console.table(imageFile);
+        CheckboxIsSelected,
+    };
 
+    console.log(projects)
     projects.push(project)
     loadProjects()
 }
 
-function timeDuration(startDate,endDate){
-    let durationResult = startDate.getMonth() - endDate.getMonth() + 12 * (endDate.getFullYear() - startDate.getFullYear());
-
-    return Math.abs(durationResult);
-}
-
-function checkboxIsSelected(){
-    let checkboxActivated = document.querySelectorAll(".language_checkbox input[type='checkbox']:checked");
-
-    let addCheckbox = []
-    
-    for(let i=0; i<checkboxActivated.length; i++){
-        addCheckbox.push(checkboxActivated[i].value);
-    }
-    return addCheckbox;
-}
-
 function loadProjects() {
-   console.log(projects);
     let containerProjects = document.getElementById("big_content")
-    
+    let valueInput = CheckboxIsSelected.length
+
     containerProjects.innerHTML = ""
 
     for (let i = 0; i < projects.length; i++) {
-       let languageType = projects[i].languages;
+
         containerProjects.innerHTML += `
-    <div class="box" id="box_project">
+        <div class="box" id="box_project">
         <div class="image_content">
             <img src="${projects[i].imageFile}" alt="" id="image_project1">
         </div>
         <div id="output_project_name">
-            <h3>${projects[i].projectName}</h3>
+            <h3><a href="blog-detail.html">${projects[i].projectName}</a></h3>
         </div>
         <div id="duration">
-            <h6>Duration : ${projects[i].duration} month(s)</h6>  
+            <h6>Duration : 3 month(s)</h6>  
         </div>
         <div id="output_description">
             <p>${projects[i].description}</p>
@@ -80,17 +71,17 @@ function loadProjects() {
         <div class="container_language_icon">
             <div class="flex_language_icon">
                 <div class="language_icon">
-                ${language_1.checked? `<img src="assets/images/node_js.png" alt="" id="node_js">`:""}
+                ${CheckboxIsSelected[0] ? `<i class="fa-brands fa-node-js fa-2x"></i>`:""}
                 </div>
                 <div class="language_icon">
-                ${language_2.checked? `<img src="assets/images/react_js.png" alt="" id="react_js">`:""}
+                ${CheckboxIsSelected[1] ? `<i class="fa-brands fa-react fa-2x"></i>`:""}
                 </div>
                 <div class="language_icon">
-                ${language_3.checked? `<img src="assets/images/vue_js.png" alt="" id="vue_js">`:""}
+                ${CheckboxIsSelected[2] ? `<i class="fa-brands fa-vuejs fa-2x"></i>`:""}
                 </div>
                 <div class="language_icon">
-                ${language_4.checked? `<img src="assets/images/typescript.png" alt="" id="typescript">`:""}
-                </div>
+                ${CheckboxIsSelected[3] ? `<i class="fa-brands fa-angular fa-2x"></i>`:""}
+                </div>  
             </div>
         </div>
         <div class="container_content_button">
@@ -98,9 +89,9 @@ function loadProjects() {
                 <button id="edit_content_button">Edit</button>
             </div>
             <div class="right_content_button">
-                <button id="delete_content_button">Delete</button>
+             <button id="delete_content_button">Delete</button>
             </div>
         </div>
-    </div>`
+    </div>`;
         }
     }
